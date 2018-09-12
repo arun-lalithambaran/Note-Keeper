cf569c37a2d0a19bd15331a5dda399946c45cdc1
Arun Lalithambaran <40573656+arun32me@users.noreply.github.com>
2018-09-12 12:19:55 +0530

    yodya

--- script.js
+++ script.js
@@ -1,48 +1,83 @@
-var dataList = [];
-var dataString = "{{}}";
-let k = 0;
-let dataCount = 0;
-function addNote() {
-  this.noteText = document.getElementById("noteText").value;
-  // dataList = window.localStorage.getItem("data");
-  if(this.noteText != "") {
-    // dataList.push(this.noteText);
-    dataStringGenerator(this.noteText);
-    // dataString = arrToJson(dataList);
-    document.getElementById("noteText").value = "";
-    window.localStorage.setItem("data", dataString);
+var notes = [];
+var noteCount = 0;
+function addNotes() {
+  let note = document.getElementById("noteText").value;
+  document.getElementById("noteText").value = "";
+
+  if(note != "") {
+    notes.push(new ToDoNote(note));
     listNotes();
-  } else {
-    alert("Empty Text!");
   }
 }
+
 function listNotes() {
-  dataString = window.localStorage.getItem("data");
-  if(dataList2 != null) {
-    let jsonData = JSON.parse(dataList2);
-    console.log(jsonData);
-    for( ; k < jsonData[0]; k++) {
-      dataList.push(jsonData[1][k]);
-      let box = document.getElementById("box_body");
-      let node = document.createElement("div");
-      node.setAttribute("class", "item_outer");
-      node.innerHTML = "<div class='item'><div class='circle_dot'></div><span>"+jsonData[1][k]+"</span></div>";
-      box.append(node);
-    }
+  for( ; noteCount < notes.length; noteCount++) {
+    notes[noteCount].show();
   }
 }
-function arrToJson(arrData) {
-  let jsonData = '{"0" : ' + '"' + arrData.length +'"' + ', "1" : {';
-  for(let i = 0; i < arrData.length; i++) {
-    jsonData += '"'+i+'" : ' + '"'+arrData[i]+'"';
-    if(i != arrData.length - 1) {
-      jsonData += ", ";
-    }
+
+function ToDoNote(noteText) {
+  this.noteText = noteText;
+  this.status = false;
+
+
+  this.show = function() {
+    console.log(this.noteText);
+    let box = document.getElementById("box_body");
+    let node = document.createElement("div");
+    node.setAttribute("class", "item_outer");
+    node.innerHTML = "<div class='item'><div class='circle_dot'></div><span>"+this.noteText+"</span></div>";
+    box.append(node);
   }
-  jsonData += "}}";
-  // console.log(jsonData);
-  return jsonData;
-}
-function dataStringGenerator(data) {
 
 }
+
+
+// var dataList = [];
+// var dataString = "{{}}";
+// let k = 0;
+// let dataCount = 0;
+// function addNote() {
+//   this.noteText = document.getElementById("noteText").value;
+//   // dataList = window.localStorage.getItem("data");
+//   if(this.noteText != "") {
+//     // dataList.push(this.noteText);
+//     dataStringGenerator(this.noteText);
+//     // dataString = arrToJson(dataList);
+//     document.getElementById("noteText").value = "";
+//     window.localStorage.setItem("data", dataString);
+//     listNotes();
+//   } else {
+//     alert("Empty Text!");
+//   }
+// }
+// function listNotes() {
+//   dataString = window.localStorage.getItem("data");
+//   if(dataList2 != null) {
+//     let jsonData = JSON.parse(dataList2);
+//     console.log(jsonData);
+//     for( ; k < jsonData[0]; k++) {
+//       dataList.push(jsonData[1][k]);
+//       let box = document.getElementById("box_body");
+//       let node = document.createElement("div");
+//       node.setAttribute("class", "item_outer");
+//       node.innerHTML = "<div class='item'><div class='circle_dot'></div><span>"+jsonData[1][k]+"</span></div>";
+//       box.append(node);
+//     }
+//   }
+// }
+// function arrToJson(arrData) {
+//   let jsonData = '{"0" : ' + '"' + arrData.length +'"' + ', "1" : {';
+//   for(let i = 0; i < arrData.length; i++) {
+//     jsonData += '"'+i+'" : ' + '"'+arrData[i]+'"';
+//     if(i != arrData.length - 1) {
+//       jsonData += ", ";
+//     }
+//   }
+//   jsonData += "}}";
+//   // console.log(jsonData);
+//   return jsonData;
+// }
+// function dataStringGenerator(data) {
+//
+// }
